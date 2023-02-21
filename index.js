977cac1f857f3f8cacf82414e30e6de7d5f61b9c
Mishal Majid <mishalmajid55@gmail.com>
2023-02-21 11:21:50 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var valueOne = 8;
+var valueTwo = 2;
+
+//Using string literals
+var str= `the value of variable "valueOne" is ${valueOne}` ;
+console.log("Variable value added:" , str);
+
+//Types of operator in javascript
+//ARITHMETIC OPERATORS
+
+//Addition Operator (+)
+var sum= valueOne + valueTwo
+console.log('the sum of valueOne and valueTwo is:',sum);
+
+//Difference Operator (-)
+var difference= valueOne - valueTwo
+console.log('The diiference Between valueOne and valueTwo is:' , difference);
+
+//Product Operator(*)
+var product= valueOne * valueTwo
+console.log ('The product of valueOne and valueTwo is:', product);
+
+//Division Operator (/)
+var division= valueOne / valueTwo
+console.log('The division of valueOne by valueTwo is:', division);
+
+//Modulus Operator (%)
+var modulus= 8 % 2
+console.log('The remainder after dividing valueOne with valueTwo is:', modulus);
+
+//Exponential Operator (**)
+var answer= 4**3
+console.log('The power of 4 to 4 is:', answer);
