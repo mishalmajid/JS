4a48c1bb0fabcc9e28c94fa1052e155df4338c86
Mishal Majid <mishalmajid55@gmail.com>
2023-02-23 03:35:52 -0800

    assignment

---
+++ 23-2-23/index.js
@@ -0,0 +1,18 @@
+let a=5;
+let b=10;
+
+
+var valueOne=15;
+var valueTwo=20;
+
+var sum= valueOne + valueTwo
+console.log('the sum of valueOne and valueTwo is:',sum);
+
+if (a>b && valueOne<valueTwo) {
+    console.log('this order is true')
+} else if (a!==b) {
+    console.log('this order is correct')
+}
+
+console.log('a is strictly euquals to my:',a===b)
+console.log('b is identity equals to me: ', b=='me' )
